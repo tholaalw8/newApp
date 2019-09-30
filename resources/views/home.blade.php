513205c9c75b778d3aeebf847092f8a4b92e2388
tholaalw8 <tholaalw8@gmail.com>
2019-10-01 01:34:51 +0500

    adding links to sign in and register in home warning!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 @guest
     <div class="container col-md-8 col-md-offset-2">
 <div class="alert alert-warning alert-dismissible show"> Welcome to the cplex software dashboard!, It seems you are not logged in </br>
-If you are a guest user please sign in or register! You may only view Movie list and ticket list as a guest</div>
+    If you are a guest user please <strong><a href="{{route('login')}}">sign in</a></strong> or <strong><a href="{{route('register')}}">register</a></a></strong>! You may only view Movie list and ticket list as a guest</div>
     </div>
     @else
 
